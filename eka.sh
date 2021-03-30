6639e9272f82dffe65c55607dfd537aa10ba8625
itservices-sudo <81635331+itservices-sudo@users.noreply.github.com>
2021-03-30 20:14:46 +0530

    Create eka.sh
    First commit

---
+++ eka.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+yum install unzip -y
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+echo $PATH | grep ~/bin      
+export PATH=~/bin:$PATH  
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
+aws --version
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+aws-iam-authenticator help
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+kubectl version --short --client
+aws eks --region ap-south-1 describe-cluster --name  cluster-name --query cluster.status
+aws eks --region ap-south-1 update-kubeconfig --name cluster-name
+kubectl get svc
+aws configure
